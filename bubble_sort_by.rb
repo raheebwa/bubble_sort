f3c817f67eb43fe5be941cb26b0e86bfc3adf480
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-10-21 23:19:20 +0300

    Created bubble sort by function

---
+++ bubble_sort_by.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def bubble_sort_by(arr)
+  swapped = false
+  n = arr.length - 2
+
+  while swapped == false
+    swap = 0
+    (0..n).each do |i|
+      diff = yield(arr[i], arr[i + 1])
+      if diff.positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap += 1
+      end
+    end
+    swapped = true unless swap.positive?
+  end
+  puts arr
+end
+
+bubble_sort_by(%w[hi hello hey howdy]) do |left, right|
+  left.length - right.length
+end
