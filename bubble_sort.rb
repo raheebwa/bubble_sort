f0e14ff2b178feb2700c3bd6e7277abd2101ef82
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-10-21 16:30:38 -0300

    Done bubbleSort fucntion

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -21,21 +21,22 @@ end procedure
 =end
 
 def bubbleSort(arr)
-    n = arr.length - 1
-    i = 0
-     while (i < n)
-
-        left = arr[i]
-        right = arr[i+1]
-
-        if arr[i] > arr[i+1]
-            arr[i] = right
-            arr[i+1] = left
-        end        
-        i+=1
-     end
-
-    puts arr
+  swapped = false
+  n = arr.length - 2
+  
+  while swapped == false
+    swap = 0
+    for i in 0..n
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swap += 1
+      end    
+    end
+    unless swap > 0
+      swapped = true
+    end
+  end
+  puts arr
 end
 
-bubbleSort([1,5,9,3])
+bubbleSort([4,6,3,78,9999,2,0,2,-3,-400])
